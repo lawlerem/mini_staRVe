b87c219d120be734f26611d583ee917aa2abd8d8
Ethan Lawler <lawlerem@dal.ca>
2022-01-09 01:03:54 -0400

    Ensemble state space model (each variable has multiple locations/particles following an ar1 process); uses atomic spd_sqrt

--- copula_tests.R
+++ copula_tests.R
@@ -1,65 +1,110 @@
+plot_w<- function(w,wt,setpar=TRUE) {
+  ns<- dim(w)[[1]]
+  nt<- dim(w)[[2]]
+  nv<- dim(w)[[3]]
+  if( setpar ) {
+    ogpar<- par(no.readonly=TRUE)
+    on.exit(par(ogpar))
+    par(mfrow=c(nv,1))
+  } else {}
+  cols<- RColorBrewer::brewer.pal(ns,"Set2")
+
+  for( v in (seq(nv)) ) {
+    plot(
+      x = c(0,nt),
+      y = c(floor(min(c(w[,,v],wt[,v]))),ceiling(max(c(w[,,v],wt[,v])))),
+      type = "n",
+      main = paste("Variable",v),
+      xlab = "Time",
+      ylab = "Value"
+    )
+    lines(
+      x = seq(nt),
+      y = wt[,v],
+      col = "black"
+    )
+    for( s in (seq(ns)) ) {
+      lines(
+        x = seq(nt),
+        y = w[s,,v],
+        col = cols[s+1]
+      )
+    }
+  }
+}
+
 library(TMB)
 compile("copula_tests.cpp","-O0 -g")
 dyn.load(dynlib("copula_tests"))
 
 nv<- 2
+ns<- 8
 nt<- 15
 npert<- 3
-nob<- nt*npert
+nob<- ns*nt*npert
 
 simdat<- list(
   y = matrix(0,nrow=nob,ncol=nv),
-  yt = rep(seq(nt)-1,each=npert)
+  ys = rep(seq(ns)-1,nt*npert),
+  yt = rep(seq(nt)-1,each=ns*npert)
 )
 simpar<- list(
-  w = matrix(0,nrow=nt,ncol=nv),
+  w = array(0,dim=c(ns,nt,nv)),
+  wt = array(0,dim=c(nt,nv)),
   working_response_pars = cbind(
-    c(5,log(3)),
-    c(5,log(2))
+    c(0,log(3)),
+    c(0,log(2))
   ),
   working_w_pars = cbind(
-    c(log(20),qlogis(0.5*c((0.7)+1))),
-    c(log(10),qlogis(0.5*c((-0.7)+1)))
+    c(log(20),qlogis(0.5*c((0.7)+1)),log(10),qlogis(0.5*c((0.2)+1))),
+    c(log(10),qlogis(0.5*c((0.5)+1)),log(10),qlogis(0.5*c((0.2)+1)))
   ),
-  logit_rho = qlogis(0.5*(c(-0.8)+1))
+  logit_rho = qlogis(0.5*(c(0.7)+1))
 )
 simmap<- list(
-  working_response_pars=factor(c(1,2,1,4))
+  working_response_pars=factor(c(NA,2,NA,4)),
+  working_w_pars = factor(c(1,2,3,NA,5,6,7,NA))
 )
 
 simobj<- MakeADFun(
   data = simdat,
   para = simpar,
-  random = "w",
+  random = c("w","wt"),
   map = simmap,
   DLL = "copula_tests",
   silent = TRUE
 )
 
 sim<- simobj$simulate()
-par(mfrow=c(2,1))
-  plot.ts(sim$y[,1])
-  plot.ts(sim$y[,2])
+plot_w(sim$w,sim$wt)
+
+# (simcheck<- checkConsistency(simobj))
+
+
+
+
 
 fitobj<- MakeADFun(
   data = list(
     y = sim$y,
+    ys = simdat$ys,
     yt = simdat$yt
   ),
   para = list(
-    w = matrix(0,nrow=nt,ncol=nv),
+    w = array(0,dim=c(ns,nt,nv)),
+    wt = array(0,dim=c(nt,nv)),
     working_response_pars = cbind(
       c(0,0),
       c(0,0)
     ),
     working_w_pars = cbind(
-      c(0,0),
-      c(0,0)
+      c(0,0,0,simpar$working_w_pars[4,1]),
+      c(0,0,0,simpar$working_w_pars[4,2])
     ),
     logit_rho = qlogis(0.5*(c(0)+1))
   ),
   map = simmap,
-  random = "w",
+  random = c("w","wt"),
   DLL = "copula_tests",
   silent = TRUE
 )
@@ -73,4 +118,11 @@ fitsdr<- sdreport(fitobj)
   Estimate = as.list(fitsdr,"Est",TRUE),
   StdError = as.list(fitsdr,"Std",TRUE)
 ))
-(fitcheck<- checkConsistency(fitobj))
+# (fitcheck<- checkConsistency(fitobj))
+
+
+
+
+par(mfcol=c(2,2))
+plot_w(sim$w,sim$wt,FALSE)
+plot_w(fitobj$report()$w,fitobj$report()$wt,FALSE)
--- copula_tests.cpp
+++ copula_tests.cpp
@@ -39,26 +39,50 @@ matrix<Type> spd_sqrt(matrix<Type> x){
 }
 
 template<class Type>
+matrix<Type> parallel_cov(Type sigma2, Type cor,int size) {
+  matrix<Type> ans(size,size);
+  for(int i=0; i<size; i++) {
+    for(int j=0; j<size; j++) {
+      // ans(i,j) = sigma2*pow(cor,abs(i-j));
+      ans(i,j) = sigma2*pow(cor,i==j ? 0 : 1);
+    }
+  }
+  return ans;
+}
+
+template<class Type>
 Type objective_function<Type>::operator() () {
-  DATA_MATRIX(y);
+  DATA_MATRIX(y); // [i,var]
+  DATA_IVECTOR(ys);
   DATA_IVECTOR(yt);
 
-  PARAMETER_MATRIX(w); // [t,var]
+  PARAMETER_ARRAY(w); // [s,t,var]
+  PARAMETER_MATRIX(wt); // [s,var]
   PARAMETER_MATRIX(working_response_pars); // [par,var]
-  PARAMETER_MATRIX(working_w_pars) // [par,var]
+  PARAMETER_MATRIX(working_w_pars) // [par,var] (par = sd, ar1, space_sd, space_cor)
   PARAMETER(logit_rho);
 
   int nob = y.rows(); // # of observations
-  int nv = y.cols(); // # of vars
-  int nt = w.rows(); // # of time points
+  int ns = w.rows(); // # of space points
+  int nt = w.col(0).cols(); // # of time points
+  int nv = w.cols(); // # of vars
+  array<Type> w_std(ns,nt,nv);
+  array<Type> w_std_transpose(nv,nt,ns);
 
   matrix<Type> response_pars = working_response_pars;
   response_pars.row(1) = exp(vector<Type>(working_response_pars.row(1))); // log_sd --> sd
 
-  matrix<Type> w_std(nt,nv);
   matrix<Type> w_pars = working_w_pars;
   w_pars.row(0) = exp(vector<Type>(working_w_pars.row(0))); // log_sd --> sd
   w_pars.row(1) = 2*invlogit(vector<Type>(working_w_pars.row(1)))-1; // logit_ar1 --> ar1
+  w_pars.row(2) = exp(vector<Type>(working_w_pars.row(2))); // log_sd --> sd (space)
+  w_pars.row(3) = 2*invlogit(vector<Type>(working_w_pars.row(3)))-1; // logit_cor --> cor (space)
+
+  vector<MVNORM_t<Type> > mvns(nv);
+  for(int v=0; v<nv; v++) {
+    MVNORM_t<Type> mvn(parallel_cov(w_pars(2,v),w_pars(3,v),ns));
+    mvns(v) = mvn;
+  }
 
   Type rho = 2*invlogit(logit_rho)-1;
   matrix<Type> R(nv,nv);
@@ -68,57 +92,86 @@ Type objective_function<Type>::operator() () {
   matrix<Type> I(nv,nv); I.setIdentity();
   MVNORM_t<Type> copula(R);
 
-
-
-
   Type nll = 0.0;
 
   for(int v=0; v<nv; v++) {
-    for(int ob=0; ob<nob; ob++) {
-      nll -= dnorm(y(ob,v),response_pars(0,v)+w(yt(ob),v),response_pars(1,v),true);
-    }
     for(int t=0; t<nt; t++) {
       Type mu, sd;
+      vector<Type> muu(ns);
       if( t == 0 ) {
         mu = 0.0;
         sd = w_pars(0,v)/sqrt(1-pow(w_pars(1,v),2));
       } else {
-        mu = w_pars(1,v)*w(t-1,v);
+        mu = w_pars(1,v)*wt(t-1,v);
         sd = w_pars(0,v);
       }
-      nll -= dnorm(w(t,v),mu,sd,true);
-      w_std(t,v) = (w(t,v) - mu) / sd;
+      nll -= dnorm(wt(t,v),mu,sd,true);
+
+      if( t == 0 ) {
+        muu.setZero();
+        muu = muu + wt(t,v);
+      } else {
+        muu = w_pars(1,v)*vector<Type>(w.col(v).col(t-1)-wt(t-1,v)) + wt(t,v);
+      }
+      nll += mvns(v)(vector<Type>(w.col(v).col(t) - muu));
+      w_std.col(v).col(t) = spd_sqrt(mvns(v).Q)*vector<Type>(w.col(v).col(t)-muu);
+    }
+    for(int ob=0; ob<nob; ob++) {
+      nll -= dnorm(y(ob,v),response_pars(0,v)+w(ys(ob),yt(ob),v),response_pars(1,v),true);
     }
   }
+  w_std_transpose = w_std.transpose();
 
-  for(int t=0; t<nt; t++) {
-    vector<Type> wrow = vector<Type>(w_std.row(t));
-    nll += Type(0.5)*logdetR + Type(0.5)*(wrow*vector<Type>(matrix<Type>(Q-I)*wrow)).sum();
+  for(int s=0; s<ns; s++) {
+    for(int t=0; t<nt; t++) {
+      vector<Type> wrow = w_std_transpose.col(s).col(t);
+      nll += Type(0.5)*logdetR + Type(0.5)*(wrow*vector<Type>(matrix<Type>(Q-I)*wrow)).sum();
+    }
   }
 
   SIMULATE{
-    for(int t=0; t<nt; t++) {
-      w_std.row(t) = copula.simulate();
+    for(int s=0; s<ns; s++) {
+      for(int t=0; t<nt; t++) {
+        w_std_transpose.col(s).col(t) = copula.simulate();
+      }
     }
+    w_std = w_std_transpose.transpose();
     for(int v=0; v<nv; v++) {
       for(int t=0; t<nt; t++) {
         Type mu, sd;
+        vector<Type> muu(ns);
         if( t == 0 ) {
           mu = 0.0;
           sd = w_pars(0,v)/sqrt(1-pow(w_pars(1,v),2));
         } else {
-          mu = w_pars(1,v)*w(t-1,v);
+          mu = w_pars(1,v)*wt(t-1,v);
           sd = w_pars(0,v);
         }
-        w(t,v) = sd*w_std(t,v) + mu;
+        wt(t,v) = rnorm(mu,sd);
+
+        if( t == 0 ) {
+          muu.setZero();
+          muu = muu + wt(t,v);
+        } else {
+          muu = w_pars(1,v)*vector<Type>(w.col(v).col(t-1)-wt(t-1,v))+wt(t,v);
+        }
+        w.col(v).col(t) = spd_sqrt(mvns(v).Sigma)*vector<Type>(w_std.col(v).col(t)) +muu;
       }
       for(int ob=0; ob<nob; ob++) {
-        y(ob,v) = rnorm(response_pars(0,v)+w(yt(ob),v),response_pars(1,v));
+        y(ob,v) = rnorm(response_pars(0,v)+w(ys(ob),yt(ob),v),response_pars(1,v));
       }
     }
     REPORT(y);
     REPORT(w);
+    REPORT(wt);
+    // REPORT(w_std);
   }
+
+  REPORT(y);
+  REPORT(w);
+  REPORT(wt);
+  REPORT(w_std);
+  REPORT(w_std_transpose);
 
   ADREPORT(response_pars);
   ADREPORT(w_pars);
